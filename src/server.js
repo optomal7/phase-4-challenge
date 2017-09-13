736f20d41b34420e1ae83971ed9028a529299043
Ryan Greenberg <hawk1138@gmail.com>
2017-09-13 08:04:44 -0700

    mostly delinted

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000
 const app = express()
 
 require('ejs')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -16,7 +17,7 @@ app.use(session({
   secret: 'this is not the greatest string in the world, this is just a secret',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: {secure: false},
 }))
 
 app.use(express.static('src/public'))
@@ -39,7 +40,7 @@ app.get('/', (req, res) => {
     if (error) {
       res.status(500).render('error', {error})
     } else {
-      let userinfo = {id: req.session.user}
+      const userinfo = {id: req.session.user}
       res.render('index', {albums, reviews, userinfo})
     }
   })
@@ -53,9 +54,9 @@ app.get('/albums/:albumID', (req, res) => {
       res.status(500).render('error', {error})
     } else {
       const album = albums[0]
-      let userinfo = {id: req.session.user}
+      const userinfo = {id: req.session.user}
       db.getReviewsByAlbumId(album.id, (error, reviews) => {
-        res.render('album', { album, reviews, userinfo})
+        res.render('album', {album, reviews, userinfo})
       })
     }
   })
@@ -65,13 +66,13 @@ app.get('/albums/:albumID/reviews/new', (req, res) => {
   const albumID = req.params.albumID
 
   if (!req.session.user) {
-    let error = {message: 'you must be signed in to post a review'}
+    const error = {message: 'you must be signed in to post a review'}
     res.render('login', {error})
   } else {
     db.getAlbumsByID(albumID, (err, albums) => {
       const album = albums[0]
-      console.log(album);
-      let userinfo = {id: req.session.user}
+      console.log(album)
+      const userinfo = {id: req.session.user}
       res.render('new-review', {album, userinfo})
     })
   }
@@ -80,20 +81,19 @@ app.get('/albums/:albumID/reviews/new', (req, res) => {
 app.post('/albums/:albumID/reviews/new', (req, res) => {
   const albumID = req.params.albumID
 
-  console.log(req.body)
-  let contents = {review: req.body.review}
+  const contents = {review: req.body.review}
 
-  if (req.body = '') {
-    let error = {message: 'You can not submit a blank review'}
+  if (req.body === '') {
+    const error = {message: 'You can not submit a blank review'}
     db.getAlbumsByID(albumID, (err, albums) => {
       const album = albums[0]
-      console.log(album);
-      let userinfo = {id: req.session.user}
+      console.log(album)
+      const userinfo = {id: req.session.user}
       res.render('new-review', {album, error, userinfo})
     })
   } else {
     console.log(req.body)
-    let userinfo = {id: req.session.user}
+    const userinfo = {id: req.session.user}
     contents.userId = userinfo.id
     contents.time = new Date()
     contents.albumId = albumID
@@ -105,30 +105,29 @@ app.post('/albums/:albumID/reviews/new', (req, res) => {
 
 app.delete('/users/reviews/:reviewID', (req, res) => {
   db.deleteReview(req.params.reviewID, (err) => {
-    let userinfo = {id: req.session.user}
+    const userinfo = {id: req.session.user}
     res.redirect(`/user/${userinfo.id}`)
   })
 })
 
 app.get('/signup', (req, res) => {
-  console.log(req.session.user);
+  console.log(req.session.user)
   res.render('signup')
 })
 
 app.post('/signup', (req, res) => {
-  console.log('loggin away -------------------');
-  console.log(req.body);
+  console.log(req.body)
   req.body.time = new Date()
   if (req.body.name === '' || req.body.email === '') {
-    let error = { message: "You must submit an email and/or name"}
+    const error = {message: 'You must submit an email and/or name'}
     res.render('signup', {error})
   } else {
     db.createUser(req.body, (error, newUser) => {
       if (error) {
-        error.message += " a.k.a. someone has already created an account with this email. Try a different one."
+        error.message += ' a.k.a. someone has already created an account with this email. Try a different one.'
         res.render('signup', {error})
       } else {
-        let id = newUser[0].id
+        const id = newUser[0].id
         req.session.user = id
         res.redirect(`/users/${id}`)
       }
@@ -136,19 +135,19 @@ app.post('/signup', (req, res) => {
   }
 })
 
-app.get('/users/:id', (req, res) =>{
-  let id = req.params.id
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id
   db.getUserById(id, (error, data) => {
     if (error) {
       res.status(500).render('error', {error})
     } else {
-      let userinfo = {id: req.session.user}
-      let user = data[0]
+      const userinfo = {id: req.session.user}
+      const user = data[0]
       db.getReviewsById(id, (error, reviews) => {
         if (error) {
           res.status(500).render('error', {error})
         } else {
-          console.log(reviews);
+          console.log(reviews)
           res.render('user', {user, reviews, userinfo})
         }
       })
@@ -157,20 +156,20 @@ app.get('/users/:id', (req, res) =>{
 })
 
 app.get('/sign-in', (req, res) => {
-  let userinfo = {id: req.session.user }
+  const userinfo = {id: req.session.user}
   res.render('login', {userinfo})
 })
 
 app.post('/sign-in', (req, res) => {
   db.getUserAccount(req.body, (error, account) => {
-    console.log(account[0] === undefined);
+    console.log(account[0] === undefined)
     if (error) {
       res.status(500).render('error', {error})
     } else if (account[0] === undefined || req.body.email !== account[0].email || req.body.password !== account[0].password) {
-      let error = {message: 'Invalid password and/or email'}
+      const error = {message: 'Invalid password and/or email'}
       res.render('login', {error})
     } else if (req.body.email === account[0].email && req.body.password === account[0].password) {
-      let userinfo = {id: account[0].id }
+      const userinfo = {id: account[0].id}
       req.session.user = userinfo.id
       res.redirect(`/users/${userinfo.id}`)
     } else {
@@ -180,13 +179,13 @@ app.post('/sign-in', (req, res) => {
 })
 
 app.get('/signout', (req, res) => {
-  req.session.destroy(function (err) {
+  req.session.destroy((err) => {
     res.redirect('/')
   })
 })
 
 app.use((err, req, res, next) => {
-  res.status(404).render('not_found', { userinfo: {}})
+  res.status(404).render('not_found', {userinfo: {}})
 })
 
 app.listen(port, () => {
